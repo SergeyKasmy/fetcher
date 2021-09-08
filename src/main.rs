c770736b472cc9e95cc561662874d5b780daf972
Sergey Kasmy <SleeplessSloth79@gmail.com>
2021-09-08 04:19:23 +0200

    Handle shutdown signals gracefully

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
+use anyhow::anyhow;
 use anyhow::Result;
 use futures::future::select_all;
+use futures::stream::StreamExt;
 use news_reader::{RssNewsReader, TwitterNewsReader};
+use signal_hook::consts as SignalTypes;
+use signal_hook_tokio::Signals;
 use std::{env, time::Duration};
 use teloxide::Bot;
+use tokio::select;
+use tokio::sync::broadcast;
 use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() -> Result<()> {
 	pretty_env_logger::init();
 
-	let mut tasks = Vec::new();
+	let (shutdown_signal_tx, _) = broadcast::channel(1);
 
+	let mut tasks = Vec::new();
 	let news_bot = Bot::new(env::var("NEWS_BOT_TOKEN")?);
 
 	{
@@ -21,13 +28,18 @@ async fn main() -> Result<()> {
 			env::var("PHORONIX_CHAT_ID")?,
 		);
 
+		let mut shutdown_signal_rx = shutdown_signal_tx.subscribe();
+
 		tasks.push(tokio::spawn(async move {
 			loop {
 				phoronix.start().await?;
-				sleep(Duration::from_secs(60 * 30)).await; // refresh every 30 mins
+				select! {
+					_ = sleep(Duration::from_secs(60 * 30)) => (), // refresh every 30 mins
+					_ = shutdown_signal_rx.recv() => break,
+				};
 			}
 
-			#[allow(unreachable_code)]
+			println!("Phoronix News Reader quiting");
 			Ok::<(), anyhow::Error>(())
 		}));
 	}
@@ -45,19 +57,50 @@ async fn main() -> Result<()> {
 		)
 		.await?;
 
+		let mut shutdown_signal_rx = shutdown_signal_tx.subscribe();
+
 		tasks.push(tokio::spawn(async move {
 			loop {
 				apex.start().await?;
-				sleep(Duration::from_secs(60 * 5)).await; // refresh every 5 mins
+				select! {
+					_ = sleep(Duration::from_secs(60 * 5)) => (), // refresh every 30 mins
+					_ = shutdown_signal_rx.recv() => break,
+				};
 			}
 
-			#[allow(unreachable_code)]
+			println!("Apex News Reader quiting");
 			Ok::<(), anyhow::Error>(())
 		}));
 	}
 
-	// NOTE: tasks don't (read: shoudn't) finish by themselves, only if there has occured an error.
-	// Wait for the first task to finish (with an error) and propagate it
-	select_all(tasks).await.0??;
+	let signals = Signals::new(&[SignalTypes::SIGINT, SignalTypes::SIGTERM])?;
+	let signals_handle = signals.handle();
+
+	tokio::spawn(async move {
+		let mut signals = signals.fuse();
+		while let Some(signal) = signals.next().await {
+			println!("Received a signal: {}", signal);
+			shutdown_signal_tx.send(true)?;
+		}
+
+		println!("Signal receiever quitting");
+		Ok::<(), anyhow::Error>(())
+	});
+
+	loop {
+		let finished_task = select_all(tasks).await;
+		match finished_task.0? {
+			Ok(_) => {
+				if !finished_task.2.is_empty() {
+					tasks = finished_task.2;
+				} else {
+					break;
+				}
+			}
+			Err(e) => return Err(anyhow!(e)),
+		}
+	}
+
+	signals_handle.close();
 	Ok(())
 }
