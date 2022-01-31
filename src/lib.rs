f242ac825425d8edf06879bfc1007f292b738df2
Sergey Kasmy <SleeplessSloth79@gmail.com>
2022-02-01 00:30:15 +0100

    Properly handle signals

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod settings;
 pub mod sink;
 pub mod source;
 
-
+/*
 use config::Config;
 use error::Error;
 use error::Result;
@@ -70,5 +70,68 @@ pub async fn run(configs: Vec<Config>) -> Result<()> {
 	}
 
 	signals_handle.close();
+	Ok(())
+}
+*/
+
+
+use futures::StreamExt;
+use futures::future::join_all;
+use signal_hook::consts as SignalTypes;
+use signal_hook_tokio::Signals;
+use std::time::Duration;
+use tokio::select;
+use tokio::sync::watch;
+use tokio::time::sleep;
+
+use crate::config::Config;
+use crate::error::Error;
+use crate::error::Result;
+
+
+#[tracing::instrument]
+pub async fn run(configs: Vec<Config>) -> Result<()> {
+	let (shutdown_tx, shutdown_rx) = watch::channel(false);
+
+	tokio::spawn(async move {
+		let signals = Signals::new(&[SignalTypes::SIGINT, SignalTypes::SIGTERM]).unwrap();
+		let signals_handle = signals.handle();
+
+		let mut signals = signals.fuse();
+
+		while signals.next().await.is_some() {
+			shutdown_tx.send(true).unwrap();
+			signals_handle.close();	// TODO: figure out wtf this is and why
+		}
+	});
+
+	let mut tasks = Vec::new();
+	for mut c in configs {
+		let mut shutdown_rx = shutdown_rx.clone();
+
+		let task = tokio::spawn(async move {
+			select! {
+				_ = async {
+					loop {
+						for m in c.source.get().await? {
+								c.sink.send(m).await?;
+						}
+
+						sleep(Duration::from_secs(c.refresh * 60 /* secs in a min */)).await;
+					}
+
+					#[allow(unreachable_code)]
+					Ok::<(), Error>(())
+				} => (),
+				_ = shutdown_rx.changed() => {
+					tracing::info!("Shutdown signal received");
+				},
+			}
+		});
+
+		tasks.push(task);
+	}
+
+	join_all(tasks).await;
 	Ok(())
 }
