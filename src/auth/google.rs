760c42f64343b18f4ad0e85438735fa2ef948e8d
Sergey Kasmy <SleeplessSloth79@gmail.com>
2022-02-07 04:00:33 +0100

    Properly license the project under MPL-2.0

--- src/auth/google.rs
+++ src/auth/google.rs
@@ -1,3 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (C) 2022, Sergey Kasmynin (https://github.com/SergeyKasmy)
+ */
+
 use serde::Deserialize;
 use std::time::{Duration, Instant};
 
