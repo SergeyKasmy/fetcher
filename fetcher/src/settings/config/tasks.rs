c25d9b8c6585408a70e1dd3320e479df56043d4b
Sergey Kasmy <SleeplessSloth79@gmail.com>
2022-07-16 21:53:43 +0200

    Correct the license header

--- fetcher/src/settings/config/tasks.rs
+++ fetcher/src/settings/config/tasks.rs
@@ -2,8 +2,6 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- *
- * Copyright (C) 2022, Sergey Kasmynin (https://github.com/SergeyKasmy)
  */
 
 // TODO: add trace logging, e.g. all config dirs, all config files, stuff like that
