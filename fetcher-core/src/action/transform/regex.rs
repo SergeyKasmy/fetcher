c74abdbfa25b4cb00fa41b5c8be0dc3799dc40b3
Sergey Kasmy <SleeplessSloth79@gmail.com>
2022-09-08 20:20:34 +0200

    Move regex transform text processing logic away from Entry API
    to be more usable elsewhere

--- fetcher-core/src/action/transform/regex.rs
+++ fetcher-core/src/action/transform/regex.rs
@@ -36,51 +36,57 @@ impl Transform for Regex {
 	type Error = RegexError;
 
 	fn transform(&self, entry: &Entry) -> Result<Vec<TransformedEntry>, Self::Error> {
-		self.transform_impl(entry).map(|e| vec![e])
-	}
-}
-
-impl Regex {
-	pub fn transform_impl(&self, entry: &Entry) -> Result<TransformedEntry, RegexError> {
-		tracing::trace!("Transforming entry {:#?}", entry);
-
+		// self.transform_impl(entry).map(|e| vec![e])
 		let body = match entry.msg.body.clone() {
-			Some(body) => match (&self.action, extract(&self.re, &body)) {
-				// return the original str if a match was found or even extracted from some reason when we are just searching
-				(Action::Find, ExtractionResult::Matched | ExtractionResult::Extracted(_)) => {
-					Some(body)
-				}
-				// return the extracted str if we are just extracting
-				(Action::Extract { .. }, ExtractionResult::Extracted(extracted_s)) => {
-					Some(extracted_s.to_owned())
-				}
-				// return the original str if we are extracting but passthrough_if_not_found is on
-				(
-					Action::Extract {
-						passthrough_if_not_found,
+			Some(body) => body,
+			None => {
+				return Ok(vec![TransformedEntry {
+					msg: TransformedMessage {
+						body: TrRes::Empty,
+						..Default::default()
 					},
-					ExtractionResult::Matched,
-				) if *passthrough_if_not_found => Some(body),
-				// return an error if we are extracting without passthrough and we haven't extracted anything
-				(
-					Action::Extract { .. },
-					ExtractionResult::Matched | ExtractionResult::NotMatched,
-				) => return Err(RegexError::CaptureGroupMissing),
-				// return nothing if we haven't found anything
-				(_, ExtractionResult::NotMatched) => None,
-			},
-			None => None,
+					..Default::default()
+				}])
+			}
 		};
 
-		tracing::trace!("Body after: {:?}", body);
+		let body = self.run(&body)?.map(ToOwned::to_owned);
 
-		Ok(TransformedEntry {
+		Ok(vec![TransformedEntry {
 			msg: TransformedMessage {
 				body: TrRes::New(body),
 				..Default::default()
 			},
 			..Default::default()
-		})
+		}])
+	}
+}
+
+impl Regex {
+	pub fn run<'a>(&self, text: &'a str) -> Result<Option<&'a str>, RegexError> {
+		let res = match (&self.action, extract(&self.re, &text)) {
+			// return the original str if a match was found or even extracted from some reason when we are just searching
+			(Action::Find, ExtractionResult::Matched | ExtractionResult::Extracted(_)) => {
+				Some(text)
+			}
+			// return the extracted str if we are just extracting
+			(Action::Extract { .. }, ExtractionResult::Extracted(extracted_s)) => Some(extracted_s),
+			// return the original str if we are extracting but passthrough_if_not_found is on
+			(
+				Action::Extract {
+					passthrough_if_not_found,
+				},
+				ExtractionResult::Matched,
+			) if *passthrough_if_not_found => Some(text),
+			// return an error if we are extracting without passthrough and we haven't extracted anything
+			(Action::Extract { .. }, ExtractionResult::Matched | ExtractionResult::NotMatched) => {
+				return Err(RegexError::CaptureGroupMissing)
+			}
+			// return nothing if we haven't found anything
+			(_, ExtractionResult::NotMatched) => None,
+		};
+
+		Ok(res)
 	}
 }
 
