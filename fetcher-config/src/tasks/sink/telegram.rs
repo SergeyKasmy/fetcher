a00ab9d347209044d5477c9085e4a983a5411339
Sergey Kasmy <SleeplessSloth79@gmail.com>
2022-10-13 01:10:49 +0200

    Set telegram sink link location to PreferTitle if not specified in cfg

--- fetcher-config/src/tasks/sink/telegram.rs
+++ fetcher-config/src/tasks/sink/telegram.rs
@@ -7,14 +7,15 @@
 use serde::{Deserialize, Serialize};
 
 use crate::tasks::external_data::ExternalData;
-use crate::Error;
-use fetcher_core::sink;
+use crate::Error as ConfigError;
+use fetcher_core::sink::telegram::LinkLocation as CLinkLocation;
+use fetcher_core::sink::Telegram as CTelegram;
 
 #[derive(Deserialize, Serialize, Debug)]
 #[serde(deny_unknown_fields)]
 pub struct Telegram {
 	chat_id: i64,
-	link_location: LinkLocation,
+	link_location: Option<LinkLocation>,
 }
 
 /// Refer to [`crate::sink::message::LinkLocation`]
@@ -26,22 +27,23 @@ pub enum LinkLocation {
 }
 
 impl Telegram {
-	pub fn parse(self, external: &dyn ExternalData) -> Result<sink::Telegram, Error> {
-		Ok(sink::Telegram::new(
+	pub fn parse(self, external: &dyn ExternalData) -> Result<CTelegram, ConfigError> {
+		Ok(CTelegram::new(
 			external
 				.telegram_bot_token()?
-				.ok_or(Error::TelegramBotTokenMissing)?,
+				.ok_or(ConfigError::TelegramBotTokenMissing)?,
 			self.chat_id,
-			self.link_location.parse(),
+			self.link_location
+				.map_or(CLinkLocation::PreferTitle, LinkLocation::parse),
 		))
 	}
 }
 
 impl LinkLocation {
-	pub fn parse(self) -> sink::telegram::LinkLocation {
+	pub fn parse(self) -> CLinkLocation {
 		match self {
-			LinkLocation::PreferTitle => sink::telegram::LinkLocation::PreferTitle,
-			LinkLocation::Bottom => sink::telegram::LinkLocation::Bottom,
+			LinkLocation::PreferTitle => CLinkLocation::PreferTitle,
+			LinkLocation::Bottom => CLinkLocation::Bottom,
 		}
 	}
 }
