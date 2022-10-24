0d0f882ee256fe8a37f6ecdb3779cc2e5bb7e652
Sergey Kasmy <SleeplessSloth79@gmail.com>
2022-10-24 16:22:41 +0200

    Count chars instead of bytes when shortening entry field

--- fetcher-core/src/action/transform/field/shorten.rs
+++ fetcher-core/src/action/transform/field/shorten.rs
@@ -9,8 +9,7 @@
 use super::TransformField;
 use crate::action::transform::result::TransformResult;
 
-use std::convert::Infallible;
-use std::iter::repeat;
+use std::{convert::Infallible, iter::repeat};
 
 /// Shorten a field to [`len`](`Shorten::len`). Makes the field completely empty if [`len`](`Shorten::len`) is 0, or trims the field to [`len`](`Shorten::len`) and adds "..." to the end
 #[derive(Debug)]
@@ -28,10 +27,10 @@ impl TransformField for Shorten {
 			None
 		} else if let Some(field) = field {
 			// pass-through the field if it's shorter than max len
-			if field.len() < self.len {
+			if field.chars().count() < self.len {
 				Some(field.to_owned())
 			} else {
-				// take self.len chars from field and append ...
+				// take self.len chars from field and append "..."
 				Some(
 					field
 						.chars()
