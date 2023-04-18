7d10f0d642d140daa289236f6066460666bf8f60
Sergey Kasmy <SleeplessSloth79@gmail.com>
2023-04-18 17:59:25 +0200

    Move App into main
    Avoid an unnecessary app mod

--- fetcher-gui/src/job/task/source/reddit.rs
+++ fetcher-gui/src/job/task/source/reddit.rs
@@ -4,7 +4,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-use crate::app::COLOR_ERROR;
+use crate::COLOR_ERROR;
 use fetcher_config::jobs::source::reddit::{self, Reddit};
 
 use egui::{ComboBox, Ui};
--- fetcher-gui/src/main.rs
+++ fetcher-gui/src/main.rs
@@ -5,13 +5,23 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-pub mod app;
+pub mod job;
 
-use self::app::App;
+use self::job::JobState;
 use fetcher_config::jobs::{action::Action, named::JobName, task::Task, Job};
 
 use eframe::NativeOptions;
+use egui::{Color32, ScrollArea, SelectableLabel};
 use std::collections::{BTreeMap, HashMap};
+
+const COLOR_ERROR: Color32 = Color32::LIGHT_RED;
+
+#[derive(Debug)]
+pub struct App {
+	pub current_job: JobName,
+	pub jobs: BTreeMap<JobName, Job>,
+	pub job_state: HashMap<JobName, JobState>,
+}
 
 fn main() {
 	let jobs = (0..100)
@@ -81,4 +91,47 @@ fn main() {
 		}),
 	)
 	.unwrap();
+}
+
+impl eframe::App for App {
+	fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+		self.top_level(ctx);
+	}
+}
+
+impl App {
+	fn top_level(&mut self, ctx: &egui::Context) {
+		egui::SidePanel::left("job list side panel").show(ctx, |ui| {
+			ScrollArea::vertical()
+				.auto_shrink([false, false])
+				.show(ui, |ui| {
+					for job_name in self.jobs.keys() {
+						// TODO: left align the text
+						if ui
+							.add_sized(
+								[ui.available_width(), 0.0],
+								SelectableLabel::new(
+									&self.current_job == job_name,
+									job_name.as_str(),
+								),
+							)
+							.clicked()
+						{
+							self.current_job = job_name.clone()
+						}
+					}
+				});
+		});
+
+		egui::CentralPanel::default().show(ctx, |ui| {
+			ScrollArea::vertical().show(ui, |ui| {
+				let job = self.jobs.entry(self.current_job.clone()).or_default();
+
+				self.job_state
+					.entry(self.current_job.clone())
+					.or_default()
+					.show(ui, self.current_job.clone(), job);
+			});
+		});
+	}
 }
