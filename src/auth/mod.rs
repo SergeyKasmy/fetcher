f30bd22924897433254d7229f76514c4b1265afa
Sergey Kasmy <sergey.kasmy@gmail.com>
2025-05-29 01:01:21 +0200

    Fix doc link

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -4,6 +4,10 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+#![allow(
+	rustdoc::broken_intra_doc_links,
+	reason = "it's broken when google-oauth2 feature isn't enabled"
+)]
 //! This module contains all external manual authentication implementations. For now it's just [`Google OAuth2`](`Google`)
 
 #[cfg(feature = "google-oauth2")]
